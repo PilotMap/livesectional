59b9ef01038672734ac6d330cfa646e74ce818c0
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-08-20 16:25:22 -0400

    fix log level, update install shells

--- log.py
+++ log.py
@@ -4,10 +4,8 @@ import logzero
 import admin
 
 from logzero import logger
-version = admin.version                 #Software version
+version = admin.version
 loglevel = logging.INFO
-loglevels = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR]
-logzero.loglevel(loglevels[loglevel])   #Choices in order; DEBUG, INFO, WARNING, ERROR
+logzero.loglevel(logging.INFO)
 logzero.logfile("./logfile.log", maxBytes=1e6, backupCount=1)
 logger.info("\n\nStartup of metar-v4.py Script, Version " + version)
-logger.info("Log Level Set To: " + str(loglevels[loglevel]))
