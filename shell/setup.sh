11bba41c46dad385429b37c6cd20ac884433f2b5
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-08-27 14:26:25 -0400

    fix requirements and cleanup

--- shell/setup.sh
+++ shell/setup.sh
@@ -3,8 +3,17 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y git emacs-nox
 sudo apt install -y python3-pip python3-venv uwsgi uwsgi-plugin-python3 nginx python3-numpy libopenjp2-7
-sudo curl  -L https://raw.githubusercontent.com/nksan/Rpi-SetWiFi-viaBluetooth/main/btwifisetInstall.sh | bash
 sudo raspi-config nonint do_i2c 0
+
+# Install BTWifi
+# sudo curl  -L https://raw.githubusercontent.com/nksan/Rpi-SetWiFi-viaBluetooth/main/btwifisetInstall.sh | bash
+
+
+# Install BerryLan
+echo -e "deb http://repository.nymea.io $(lsb_release -s -c) main" | sudo tee /etc/apt/sources.list.d/nymea.list
+sudo wget -O /etc/apt/trusted.gpg.d/nymea.gpg https://repository.nymea.io/nymea.gpg
+sudo apt-get update
+sudo apt-get install nymea-networkmanager dirmngr
 
 python3 -m venv --system-site-packages livesectional
 cd livesectional
