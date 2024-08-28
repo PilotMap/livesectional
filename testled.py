ca1cc434ff4d5a78f2733bcd00cf6dd042d8fc95
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-08-28 13:31:17 -0400

    update led tests

--- testled.py
+++ testled.py
@@ -1,3 +1,4 @@
+import time
 import subprocess
 from leds import LedStrip
 
@@ -5,7 +6,11 @@ if __name__ == '__main__':
     # Create NeoPixel object with appropriate configuration.
     subprocess.run(['systemctl', 'stop', 'metar-v4'], capture_output=True)
     strip = LedStrip()
-    strip.rainbow(3, 5)
+    for i in range(strip.number):
+        strip.setPixelColor(i,0xFF0000)
+        strip.show_pixels()
+        time.sleep(.1)
+
     #strip.orange()
     subprocess.run(['systemctl', 'start', 'metar-v4'],capture_output=True)
 
