b69d3269e01647efec0de5a3b21baa175b870e35
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-08-28 13:00:29 -0400

    update led tests

--- testled.py
+++ testled.py
@@ -4,4 +4,5 @@ from leds import LedStrip
 if __name__ == '__main__':
     # Create NeoPixel object with appropriate configuration.
     strip = LedStrip()
-    strip.rainbow(3,1)
+    strip.rainbow(3,5)
+    strip.orange()
