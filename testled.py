738bbaea3fe3431241220c5ce76e999d5898269f
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-08-28 13:20:33 -0400

    update led tests

--- testled.py
+++ testled.py
@@ -1,8 +1,10 @@
-
+import subprocess
 from leds import LedStrip
 
 if __name__ == '__main__':
     # Create NeoPixel object with appropriate configuration.
+    subprocess.run("systemctl stop metar-v4")
     strip = LedStrip()
-    strip.rainbow(3,5)
-    strip.orange()
+    strip.rainbow(3, 5)
+    #strip.orange()
+    subprocess.run("systemctl start metar-v4")
