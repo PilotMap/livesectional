0c051c7c2f1ff7d541d5d8576e0d9c91034df565
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-09-09 22:47:05 -0400

    fix number of leds

--- testled.py
+++ testled.py
@@ -5,7 +5,7 @@ from leds import LedStrip
 if __name__ == '__main__':
     # Create NeoPixel object with appropriate configuration.
     subprocess.run(['systemctl', 'stop', 'metar-v4'], capture_output=True)
-    strip = LedStrip()
+    strip = LedStrip(200)
     for i in range(strip.number):
         strip.set_pixel_color(i,0xFF0000)
         strip.show_pixels()
