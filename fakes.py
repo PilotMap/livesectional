4e568bfe422814f7ea24fcacbdc0aeb4eea281fd
Pete Douma <86552+CrabbyPete@users.noreply.github.com>
2024-09-09 22:42:26 -0400

    fix number of leds

---
+++ fakes.py
@@ -0,0 +1,34 @@
+
+
+class GPIO:
+    def setmode(mode):
+        return
+
+    def setup(self, **args):
+        return
+
+    def input(self):
+        return 1
+
+def Color(r, g, b):
+    """
+    The code has Color all over, but never defined. It just returns a set to pass back to the
+    Adafruit library calls
+    """
+    return int("0x{:02x}{:02x}{:02x}".format(r, g, b), 16)
+
+class PixelStrip:
+
+    def init(self, *args):
+        return
+
+    def begin(self):
+        return
+
+    def NumPixels(self):
+        return 100
+
+    def setPixelColor(self, *args):
+        return
+
+    def set
--- leds.py
+++ leds.py
@@ -6,7 +6,7 @@ except ModuleNotFoundError:
     from fakes import PixelStrip, Color
 
 # LED strip configuration:
-LED_COUNT      = 60       # Number of LED pixels.
+LED_COUNT      = 500      # Number of LED pixels.
 LED_PIN        = 18       # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000   # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10       # DMA channel to use for generating signal (try 10)
